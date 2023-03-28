1fa17527bc4eadd607e36d54b18e21ce8c7ae5fc
Fancy Pants <edaily94@gmail.com>
2023-03-27 18:13:22 -0700

    first pass at JS. Able to save to local storage

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+//Current Time
+var locationBox = document.getElementById('locationBox');
+var goButton = document.getElementById('go-button');
+
+var now = dayjs();
+
+$('#headerTime').text(now.format('MMM D, YYYY, h:mm a'));
+
+
+function updateTime() {
+  var now = dayjs();
+  $('#headerTime').text(now.format('MMM D, YYYY h:mm a'));
+}
+
+setInterval(updateTime, 1000);
+
+//saving city to local storage
+
+goButton.addEventListener('click', () => {
+    var userCity = locationBox.value;
+  
+    localStorage.setItem('city', userCity);
+    console.log(userCity)
+
+    var heroCity = localStorage.getItem('userCity')
+    var cityTitle = document.createElement('h2')
+    cityTitle.textContent =heroCity
+    var currentDay = document.getElementById('currentCity')
+    currentDay.appendChild(cityTitle)
+
+  });
+
+// grabbing city from local storage, passing it to current-day box
+
+
+
+  // need to grab user city and get the lat & long
+
+  //need to append info from the object into the current-day box
+
+  //then need to dynamically generate five cards for each day. A day is 8 items in the array
+  
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=34.052238&lon=-118.243340&appid=d84b2a8be5d2ca1ba21dcd598e80ef62')
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
